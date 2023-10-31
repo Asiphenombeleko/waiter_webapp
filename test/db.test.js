1b3cb6370d257eb612d9e929e7d6893b49eb0f20
Asiphenombeleko <asiphenombeleko33@gmail.com>
2023-10-31 12:27:44 +0200

    added a test for booking a shift

--- test/db.test.js
+++ test/db.test.js
@@ -18,57 +18,39 @@ describe("Waiter Data Functions", () => {
   });
 
   it('should insert and retrieve usernames', async () => {
-    const usernames = [
-      'Nicholas',
-      'Katleho',
-      'Yamisa',
-      'Asisipho',
-      'Saffah',
-    ];
-  
     try {
-      // Insert the usernames into the database
-      for (const username of usernames) {
-        await data.insertUsername(username);
-      }
+      const usernames = await data.getNames();
   
-      // Retrieve the list of usernames
-      const retrievedUsernames = await data.getNames();
+      assert.notStrictEqual(usernames.length, 0);
   
-      // Check if the retrieved usernames include the test usernames
-      assert.deepStrictEqual(retrievedUsernames, usernames.map((username) => ({ username })));
+      usernames.forEach(username => {
+        assert.strictEqual(typeof username.username, 'string');
+        assert.notStrictEqual(username.username, '');
+      });
     } catch (error) {
-      // Handle any errors
       throw error;
     }
   });
 
   it('should associate waiters with weekdays', async function () {
-    this.timeout(5000); // Set a timeout for this test
-  
-    // Define a test username and weekdayIds
+    this.timeout(5000); 
+ 
     const username = 'Nicholas';
     const weekdayIds = [1, 2, 3, 4, 5, 6, 7];
   
     try {
-      // Insert the username into the database
       await data.insertUsername(username);
   
-      // Retrieve the user's ID
       const userId = await data.getUserId(username);
   
-      // Associate the user with weekdays
       await data.bookShift(userId, weekdayIds);
   
-      // Retrieve the selected weekdays for the user
       const selectedDays = await data.getWaiterSelectedDays(username);
   
-      // Verify that the selectedDays match the provided weekdayIds
       const selectedDayIds = selectedDays.map((day) => day.id);
   
       assert.deepStrictEqual(selectedDayIds, weekdayIds);
     } catch (error) {
-      // Handle any errors
       throw error;
     }
   });
@@ -89,5 +71,34 @@ describe("Waiter Data Functions", () => {
   it('should return "danger" for more than 3 waiters booked for the day', function () {
     const color = factory.addColor(4);
     assert.equal(color, "danger");
+  });
+});
+describe('bookShift',async () => {
+ 
+  before(async () => {
+    await data.reset();
+  });
+
+  let testUsername = 'Nicholas';
+
+  it('should book shifts for a user', async () => {
+  
+    const userId = await data.getUserId(testUsername);
+
+    const weekdays = await data.showDays();
+
+    const selectedWeekdays = [weekdays[0].id, weekdays[1].id];
+
+    await data.bookShift({ id: userId }, selectedWeekdays);
+
+    const bookedWeekdays = await db.any(
+      'SELECT weekday_id FROM user_weekday WHERE username_id = $1',
+      [userId]
+    );
+
+    assert.deepStrictEqual(
+      bookedWeekdays.map(entry => entry.weekday_id),
+      selectedWeekdays
+    );
   });
 });
